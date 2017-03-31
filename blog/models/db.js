1eb176e48f70e782840cdf1b98b53a132a908cb6
panda <841274634@qq.com>
2017-03-31 17:26:19 +0800

    del blog

--- blog/models/db.js
+++
@@ -1,4 +0,0 @@
-﻿var settings = require('../settings'),
-    Db = require('mongodb').Db,
-    Server = require('mongodb').Server;
-module.exports = new Db(settings.db, new Server(settings.host, 27017), {safe: true});
