cc150d5bb1084d88dce2d69ff59db90ad831d6cf
zhaoxin <841274634@qq.com>
2017-04-03 21:37:25 +0800

    update mongodb

--- Node/test/test.js
+++ Node/test/test.js
@@ -1,3 +1,12 @@
-var frame = require('../modules/frame');
-console.log(frame.format(new Date(), 'yyy-MM-dd 星期w'));
-console.log(frame.format('发的{0}萨芬{1}士大夫大{0}厦', 'a', 'b'));
+var DB = require('../models/mongodb');
+DB.Insert('user', { "name": "zx", "url": "www.baidu.com" }, function() {
+    console.log('数据插入成功');
+
+});
+DB.Find('user', { 'name': 'zx' }, { 'name': 1, url: 1 }, null, null, function(res) {
+    console.log(res);
+});
+
+DB.Find('user', { 'name': 'zx' }, { 'name': 1 }, 2, 2, function(res) {
+    console.log(res);
+});
