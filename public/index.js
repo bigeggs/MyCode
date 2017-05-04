6bbae7d1e6a4ae0246890872873d525d532dd889
zhaoxin <841274634@qq.com>
2017-05-04 23:13:20 +0800

    update

--- public/index.js
+++ public/index.js
@@ -2,12 +2,6 @@ webpackJsonp([0],[
 /* 0 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
 var body = document.body;
 var h1 = document.createElement("h1");
 h1.innerHTML = 'hello world,this is moduleAPart';
@@ -18,7 +12,7 @@ p.innerHTML = '<p>（此文件路径' + js[js.length - 1].src + ') < /p>';
 body.appendChild(p);
 
 /***/ }),
-/* 2 */
+/* 1 */
 /***/ (function(module, exports) {
 
 var body = document.body;
@@ -31,7 +25,7 @@ p.innerHTML = '<p>（此文件路径' + js[js.length - 1].src + ') < /p>';
 body.appendChild(p);
 
 /***/ }),
-/* 3 */
+/* 2 */
 /***/ (function(module, exports) {
 
 var body = document.body;
@@ -44,13 +38,19 @@ p.innerHTML = '<p>（此文件路径' + js[js.length - 1].src + ') < /p>';
 body.appendChild(p);
 
 /***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
 /* 4 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var mA = __webpack_require__(1); //alias path
-var mB = __webpack_require__(2); //alias path
-var mC = __webpack_require__(3); //relative path
-var cssA = __webpack_require__(0); //relative pathnpm
+var mA = __webpack_require__(0); //alias path
+var mB = __webpack_require__(1); //alias path
+var mC = __webpack_require__(2); //relative path
+var cssA = __webpack_require__(3); //relative pathnpm
 
 /***/ })
 ],[4]);
